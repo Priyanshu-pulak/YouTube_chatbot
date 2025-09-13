44aa3c10413d55ce4c4a4aa1efd34d31d81999d1
Priyanshu Pulak <priyanshu.pulak11@gmail.com>
2025-09-13 19:07:43 +0530

    change embedding model

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,7 @@
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
@@ -10,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 chat_model = ChatGoogleGenerativeAI(model = 'gemini-2.5-flash-lite')
-embedding_model = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+embedding_model = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all-MiniLM-L6-v2')
 parser = StrOutputParser()
 
 def get_video_id(url: str) -> str:
