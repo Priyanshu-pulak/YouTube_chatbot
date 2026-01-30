ee4d989ff02b0963f6e474cf76f6710749913e8b
Priyanshu Pulak <priyanshu.pulak11@gmail.com>
2026-01-30 23:27:31 +0530

    refactor: replace load_summarize_chain with custom summarization logic

--- src/vector_stores/summary_vector_store.py
+++ src/vector_stores/summary_vector_store.py
@@ -1,27 +1,33 @@
 from langchain_community.vectorstores import FAISS
-from langchain.schema import Document
-from langchain.chains.summarize import load_summarize_chain
+from langchain_core.documents import Document
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 import time
 
-def summarize_chunks(chunks, chat_model):
-    chunk_summary_chain = load_summarize_chain(chat_model, chain_type='stuff')
+def summarize_chunks(chunks: list[Document], chat_model) -> list[Document]:
+    prompt = ChatPromptTemplate.from_template("Summarize the following content concisely:\n\n{context}")
+    
+    summarize_chain = prompt | chat_model | StrOutputParser()
+    
     final_summaries = []
+    
     for i, chunk in enumerate(chunks):
-        print(f"Summarizing chunk {i + 1}/{len(chunks)}:")
-        summary_output = chunk_summary_chain.invoke([chunk])
-        summary_text = summary_output['output_text']
-        final_summaries.append(Document(page_content=summary_text))
-        if(i != len(chunks) - 1):
+        print(f"Summarizing chunk {i + 1}/{len(chunks)}...")
+        
+        summary_text = summarize_chain.invoke({"context": chunk.page_content})
+        
+        final_summaries.append(Document(page_content = summary_text))
+        
+        if i != len(chunks) - 1:
             time.sleep(8)
 
     print("All chunks summarized successfully!")
     return final_summaries
 
-def build_summary_vector_store(chunks, chat_model, embedding_model):
+def build_summary_vector_store(chunks: list[Document], chat_model, embedding_model) -> FAISS:
     final_summaries = summarize_chunks(chunks, chat_model)
     summary_vector_store = FAISS.from_documents(
-        documents = final_summaries,
-        embedding = embedding_model
+        documents=final_summaries,
+        embedding=embedding_model
     )
-
     return summary_vector_store
